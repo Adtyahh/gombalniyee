1b7c1ef86e220cc1bf6e9b6e73e4062b2e1f0795
Aditya Hilmi Hidayat <aditlisa.la@gmail.com>
2025-11-09 19:24:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ const btnIya = document.getElementById('btn-iya');
 const btnNggak = document.getElementById('btn-nggak');
 const pesanCinta = document.getElementById('pesan-cinta');
 const pertanyaan = document.getElementById('pertanyaan');
+const container = document.querySelector('.container');
 
 btnIya.addEventListener('click', function() {
-  
     pesanCinta.style.display = 'block';
-
     pertanyaan.style.display = 'none';
     btnIya.style.display = 'none';
     btnNggak.style.display = 'none';
@@ -14,15 +13,18 @@ btnIya.addEventListener('click', function() {
 
 btnNggak.addEventListener('mouseover', function() {
    
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
+    const containerWidth = container.clientWidth;
+    const containerHeight = container.clientHeight;
 
     const btnWidth = btnNggak.offsetWidth;
     const btnHeight = btnNggak.offsetHeight;
 
-    const newTop = Math.random() * (windowHeight - btnHeight);
-    const newLeft = Math.random() * (windowWidth - btnWidth);
+    const padding = 30; 
+    const newTop = Math.random() * (containerHeight - btnHeight - padding);
+    const newLeft = Math.random() * (containerWidth - btnWidth - padding);
 
     btnNggak.style.top = newTop + 'px';
     btnNggak.style.left = newLeft + 'px';
+
 });
+
